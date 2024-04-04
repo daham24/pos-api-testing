2c9c2f437a69789e3a73e35167d107670769f688
daham24 <bhanukadaham98@gmail.com>
2024-04-04 20:49:49 +0530

    Update 1

--- controller/UserController.js
+++ controller/UserController.js
@@ -27,25 +27,25 @@ const register = (req, res) => {
     }
   });
 
-  // bcrypt.hash(req.body.password, salt, function (err, hash) {
-  //   if (err) {
-  //     return res.status(500).json(err);
-  //   }
-  //   const user = new userSchema({
-  //     fullName: req.body.fullName,
-  //     password: hash,
-  //     email: req.body.email,
-  //     activeState: req.body.activeState,
-  //   });
-  //   user
-  //     .save()
-  //     .then((saveResponse) => {
-  //       return res.status(201).json({ message: "Saved!" });
-  //     })
-  //     .catch((error) => {
-  //       return res.status(500).json(error);
-  //     });
-  // });
+  bcrypt.hash(req.body.password, salt, function (err, hash) {
+    if (err) {
+      return res.status(500).json(err);
+    }
+    const user = new userSchema({
+      fullName: req.body.fullName,
+      password: hash,
+      email: req.body.email,
+      activeState: req.body.activeState,
+    });
+    user
+      .save()
+      .then((saveResponse) => {
+        return res.status(201).json({ message: "Saved!" });
+      })
+      .catch((error) => {
+        return res.status(500).json(error);
+      });
+  });
 };
 
 const login = (req, res) => {};
